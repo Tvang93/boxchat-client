1aed4326f65fcde683ca54c4e42e2cf25206b3c0
Tvang93 <vangt39@gmail.com>
2025-09-15 21:49:01 -0700

    login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-
-
 export default function Home() {
   return (
-    <div>
-      PlaceHolder
-    </div>
+    <main className="grid grid-cols-12 min-h-screen bg-amber-200">
+      <div className="col-span-3 col-start-10 flex flex-col justify-center p-4 bg-white">
+        <div className="flex items-center justify-center flex-col mb-4">
+          <h1>BoxChat</h1>
+          <p>Welcome to BoxChat!</p>
+        </div>
+        <div className="flex items-center justify-center flex-col mb-4">
+          <input className="border-1 ps-1 rounded mb-2" type="text" placeholder="Username"/>
+          <input className="border-1 ps-1 rounded mb-2" type="text" placeholder="Password"/>
+          <button className="bg-slate-400 w-[50%] rounded">Submit</button>
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <h3>No Account?</h3>
+          <button className="text-blue-500"><u>Create an Account</u></button>
+        </div>
+      </div>
+    </main>
   );
 }
