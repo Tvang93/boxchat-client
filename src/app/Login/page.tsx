6638b9a1c88f81e273e1f5e9d5d648df0f848815
Tvang93 <vangt39@gmail.com>
2025-09-17 20:56:31 -0700

    login

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center flex-col mb-4">
+        <input
+          className="border-1 ps-1 rounded mb-2"
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          className="border-1 ps-1 rounded mb-2"
+          type="text"
+          placeholder="Password"
+        />
+        <button className="bg-slate-400 w-[50%] rounded">Submit</button>
+      </div>
+      <div className="flex items-center justify-center flex-col">
+        <h3>No Account?</h3>
+        <button className="text-blue-500">
+          <u>Create an Account</u>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
